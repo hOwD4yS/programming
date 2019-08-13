d8082cf494ed062e9358b10aacef25cb49c52553
hOwD4yS <37980396+hOwD4yS@users.noreply.github.com>
2019-08-13 14:30:26 +0900

    Delete getdecompile.py

--- ida_code_diffing/utils_/getdecompile.py
+++
@@ -1,5 +0,0 @@
-import os , sys
-
-def decompile(b1,b2) : 
-	#PY_PATH = os.path.abspath(__file__)
-	#IDA_CMD = lambda x:'"{ida_path}" -c -A -S{py_path} {bin_path} 1>/dev/null 2>/dev/null'.format(ida_path=IDA_PATH, py_path=PY_PATH, bin_path=x)
